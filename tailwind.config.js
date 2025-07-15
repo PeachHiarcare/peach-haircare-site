eb9101d02a146a1c36c3bf0ee44d9690a6c953b7
PeachHiarcare <bellezahairdressing@gmail.com>
2025-07-15 15:24:25 +0200

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        peach: {
+          50: "#fff4f0",
+          100: "#ffe4db",
+          200: "#ffc7b3",
+          600: "#ff885e",
+          700: "#e76f49",
+          900: "#4a1f0f",
+        },
+      },
+    },
+  },
+  plugins: [],
+}
