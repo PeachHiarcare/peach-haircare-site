cefa2b84d0dd89704abfb617027097d94ba1d290
PeachHiarcare <bellezahairdressing@gmail.com>
2025-07-16 12:24:47 +0200

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to Peach Haircare</h1>
+      <p>Natural keratin treatments with peach perfection 🍑</p>
+    </div>
+  );
+}
